938745bb49e961d7f878c828cad4b39883d51adf
Quantum Cat <veronika.netch@gmail.com>
2019-10-11 22:53:54 +0300

    General review.
    - No working code changes, comments only.
    - Turns out you can only leave comments on pull requests, so a separate branch it is.
    - A general comment: many extras spaces. Not like it's a big deal, but hey, if we're going to be perfectionists.

--- field.cpp
+++ field.cpp
@@ -1,6 +1,9 @@
 #include "field.h"
 #include <QTime>
 #include <QTimer>
+// Will you require timers both in MainWindow and Field?
+// If think that field doesn't really need it.
+// If not, you can remove extra imports.
 #include <QtWidgets>
 #include "mainwindow.h"
 #include <iostream>
@@ -17,16 +20,29 @@ Field::Field(QWidget *parent, char difficulty) : QWidget(parent){
     this->difficulty = difficulty;
 }
 
+// It is possible to suppress the warning you need to ignore so it won't distract.
+// Like this:
+// void Field::paintEvent(__attribute__((unused))QPaintEvent *Event) {
 void Field::paintEvent(QPaintEvent *Event) {
 
     QPainter painter(this);
     QPixmap flag = QPixmap(":/new/images/flag.png");
+    // And yes, rename the folder pls.
     //painter.drawPixmap(10,10,50,50, flag);
 
     painter.fillRect(0, 0, fieldPixelSize, fieldPixelSize, block_color);
 
     //difficulty = 'h';//temporary
+    // You don't need this one anymore, so you can remove it.
+    // In general, it's better not to leave code on comment for a long time.
+    // You can always retrieve it back from Git. That's what it is for.
+    // Same applies to code comments above and below.
 
+    // This code just asksfor switch/case instead.
+    /// Also, why do you find the difficulty and the block size EVERY time we call paintEvent?
+    /// It's not like it can change any time.
+    /// Better to find this value in the class constructor.
+    /// Or even in a separate function that is called from the constructor, if you prefer.
     if (difficulty == 'e'){
         fieldSize = 10;
     }
@@ -47,8 +63,6 @@ void Field::paintEvent(QPaintEvent *Event) {
 
     painter.drawPixmap(0, 0, blockSize, blockSize, flag);
     //QPoint pos = QCursor::pos();
-
-
 }
 
 void Field::mousePressEvent(QMouseEvent *e){
